a6730ae969731c9c3824577f07ddc7489a8e371c
Anna <anna.safavi@yahoo.com>
2023-01-12 22:23:21 -0800

    youtube fake initial files

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+import { logo } from "../utils/constants";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SearchBar from "./SearchBar";
+const Navigation = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link
+      to="/"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        color: "white",
+        paddingLeft: "30px",
+      }}
+    >
+      <PlayArrowIcon
+        sx={{
+          fontSize: "70px",
+        }}
+      />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navigation;
